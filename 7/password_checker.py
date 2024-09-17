42229a300cf01b0884cea19fc92b873dec67f240
Dan <148991907+danielb1000@users.noreply.github.com>
2024-09-17 07:26:42 +0100

    added compiled regex

--- 7/password_checker.py
+++ 7/password_checker.py
@@ -6,6 +6,7 @@ You may need to test the string against multiple regex patterns to validate its 
 
 import re
 
+# not using compiled regex
 def is_strong_password(password: str) -> bool:
     # 8 chars
     if not re.search(r'.{8,100}', password):
@@ -25,7 +26,38 @@ def is_strong_password(password: str) -> bool:
 
     return True
 
+
+# using compiled regular expressions
+pattern_length = re.compile(r'.{8,100}')
+pattern_lowercase = re.compile(r'[a-z]')
+pattern_uppercase = re.compile(r'[A-Z]')
+pattern_digit = re.compile(r'\d')
+
+def is_strong_password_compiled(password: str) -> bool:
+    # 8 chars
+    if not pattern_length.search(password):
+        return False
+    
+    # 1 <= lowercase
+    if not pattern_lowercase.search(password):
+        return False
+
+    # 1 <= uppercase
+    if not pattern_uppercase.search(password):
+        return False
+
+    # 1 <= digit
+    if not pattern_digit.search(password):
+        return False
+
+    return True
+
 print(is_strong_password("tomorrow"))
 print(is_strong_password("tomorrow1"))
 print(is_strong_password("Tomorrow"))
 print(is_strong_password("Tomorrow1"))
+print()
+print(is_strong_password_compiled("tomorrow"))
+print(is_strong_password_compiled("tomorrow1"))
+print(is_strong_password_compiled("Tomorrow"))
+print(is_strong_password_compiled("Tomorrow1"))
